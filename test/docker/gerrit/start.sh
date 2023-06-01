22e0961fbdde7538f9b92964cfa64f45075dbedb
Yash Chaturvedi <yash.chaturvedi@linaro.org>
2023-06-01 11:35:53 +0530

    fix MailException occuring in gerrit container
    MailException is observed in gerrit container when set-account command
    is executed, fix the issue by updating gerrit.config file in gerrit
    container so that gerrit do not send email messages.
    
    Change-Id: Ib1633088c732a9ab8f6e3453b0d747680ed7c31b

--- test/docker/gerrit/start.sh
+++ test/docker/gerrit/start.sh
@@ -2,6 +2,8 @@
 
 git config -f "$GERRIT_SITE/etc/gerrit.config" \
     operator-alias.change.independson "in_depends-on"
+git config -f "$GERRIT_SITE/etc/gerrit.config" \
+    sendemail.enable "false"
 
 echo "Initializing Gerrit site ..."
 java -jar "$GERRIT_SITE/bin/gerrit.war" init -d "$GERRIT_SITE" --batch
